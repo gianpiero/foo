c455ef0508856e82b8ddcfae853e317fd1593c32
Gianpiero Napoli <magopieri@gmail.com>
2016-03-25 14:08:19 -0700

    Texting brew

---
+++ brew/rtiutils.rb
@@ -0,0 +1,12 @@
+class Rtiutils < Formula
+  desc "simple utilities for RTI Connext DDS"
+  homepage "http://www.rti.com"
+  url "https://github.com/gianpiero/foo/raw/master/brew/pkgs/rtiutils.tar.gz"
+  sha256 "9a187ed7b900664bf64b0aa7465faa241ec93b59cee15f8faa4ddd82245f76b7"
+
+  bottle :unneeded
+
+  def install
+    bin.install "rtiddsspy", "rtiddsping", "rtiddsprototyper"
+  end
+end
