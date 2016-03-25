0cd94b84f2867d403be8bde71fe73ed10f7f4688
Gianpiero Napoli <magopieri@gmail.com>
2016-03-25 14:15:37 -0700

    added version in formula

--- brew/rtiutils.rb
+++ brew/rtiutils.rb
@@ -2,6 +2,7 @@ class Rtiutils < Formula
   desc "simple utilities for RTI Connext DDS"
   homepage "http://www.rti.com"
   url "https://github.com/gianpiero/foo/raw/master/brew/pkgs/rtiutils.tar.gz"
+  version "5.2.1"
   sha256 "9a187ed7b900664bf64b0aa7465faa241ec93b59cee15f8faa4ddd82245f76b7"
 
   bottle :unneeded
