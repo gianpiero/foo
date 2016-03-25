9541d5b323d1b34171a84e46e84bc55293cf08a1
Gianpiero Napoli <magopieri@gmail.com>
2016-03-25 14:22:22 -0700

    fixed url

--- brew/rtiutils.rb
+++ brew/rtiutils.rb
@@ -1,7 +1,7 @@
 class Rtiutils < Formula
   desc "simple utilities for RTI Connext DDS"
   homepage "http://www.rti.com"
-  url "https://github.com/gianpiero/foo/raw/master/brew/pkgs/rtiutils-5.2.1.tar.gz"
+  url "https://github.com/gianpiero/foo/raw/master/brew/pkgs/rtiddsutils-5.2.1.tar.gz"
   sha256 "9a187ed7b900664bf64b0aa7465faa241ec93b59cee15f8faa4ddd82245f76b7"
 
   bottle :unneeded
